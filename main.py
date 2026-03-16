2e603af81050b484a816e5f5c7df4847ccd1b211
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2026-03-16 16:20:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+#1-misol
+a = [10, 20, 30, 40]
+print(a)
+
+b = len(a)
+print(f"a ning uzunligi: {b}")
+
+#2-misol
+a = [5, 8, 12, 7]
+print(a)
+
+#3-misol
+mevalar = ["olma", "anor", "uzum"]
+print(mevalar)
+
+#4-misol
+sonlar = [1, 2, 3, 4, 5]
+print(sonlar)
+
+print(sonlar[-1])
+
+#5-misol
+a = [11, 22, 33]
+print(a)
+print(a[0])
